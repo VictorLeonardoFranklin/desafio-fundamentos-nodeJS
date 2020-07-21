02ea55fccfeef4a08f0506f165e6f09f207a8424
Victor Franklin <VictorLeonardoFranklin@gmail.com>
2020-07-21 17:04:58 -0300

    [Fix] teste conceitos node

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface createTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -15,14 +20,41 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const balance = this.transactions.reduce(
+      (soma: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            soma.income += transaction.value;
+            break;
+          case 'outcome':
+            soma.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        soma.total = soma.income - soma.outcome;
+        return soma;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
+  public create({ title, type, value }: createTransaction): Transaction {
     // TODO
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
